0249cc4aeaa9fa340550e973fadb9d78ec15e66d
korallion <karl.jakelski@gmail.com>
2024-10-23 10:21:37 +0100

    add frame limit

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,6 +21,8 @@ def main():
         screen.fill("black")
 
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
