dbd0e09deaba5f05785d386d7ccc13b134756da3
korallion <karl.jakelski@gmail.com>
2024-10-23 20:02:40 +0100

    added player display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -12,6 +13,10 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while running:
         for event in pygame.event.get():
@@ -19,7 +24,8 @@ def main():
                 return
             
         screen.fill("black")
-
+        player.draw(screen)
+        
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
